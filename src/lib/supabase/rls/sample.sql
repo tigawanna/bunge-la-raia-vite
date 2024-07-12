dbcfed4bcaefc17a88a893dc5f3213ea4517ea94
tigawanna <denniskinuthiaw@gmail.com>
2024-07-12 21:17:49 +0300

    push

--- src/lib/supabase/rls/sample.sql
+++ src/lib/supabase/rls/sample.sql
@@ -1,27 +1,68 @@
-CREATE policy "Enable delete for users based on user_id"
-on "public"."admin" 
-AS PERMISSIVE
-FOR SeLECT
-TO public
+--  available methods SELECT, UPDATE, DELETE, INSERT OR ALL
+
 -- only allow action for any user
-using (THEN);
---  allow for authenticated users only
-with check (TRUE);
--- only allow action if user_id matches authenticated user.id
-using ( ( SELECT auth.uid () ) = user_id )
-
---  allow based on logged in user's email email
-using (SELECT auth.jwt () ) ->> 'email' = email
-with check (SELECT auth.jwt () ) ->> 'email' = email;
-
-
---  based on joins on other tables 
-CREATE policy "Policy for writing to readonly tables (mps,conties,constituencies,wards)" on "public"."admin" AS PERMISSIVE
-FOR UPDATE
+CREATE policy "Enable for everyone"
+on "public"."admin"
+AS PERMISSIVE
+FOR SELECT -- or UPDATE , DELETE, INSERT or ALL
 TO public
-using ( (SELECT auth.uid ()) IN (SELECT user_id FROM "admin" WHERE "user_id" = id));
---  using postgres functions
+using (TRUE);
 
+-- enable for authenticated users
+CREATE policy "enable for authenticated users"
+on "public"."admin"
+AS PERMISSIVE
+FOR SELECT -- or UPDATE , DELETE, INSERT or ALL
+to authenticated
+with check (true);
+
+-- only allow update for users based on email
+create policy "Enable action for users based on email"
+on "public"."admin" AS PERMISSIVE
+FOR SELECT -- or UPDATE , DELETE, INSERT or ALL
+using (
+  (select auth.jwt()) ->> 'email' = email
+) with check (
+  (select auth.jwt()) ->> 'email' = email
+);
+
+-- only allow action if user_id matches authenticated user.id
+CREATE policy "Enable action if user_id matches authenticated user.id"
+on "public"."admin"
+AS PERMISSIVE
+FOR SELECT -- or UPDATE , DELETE, INSERT or ALL
+TO public
+USING ( 
+    ( SELECT auth.uid () ) = user_id )
+
+
+-- authorize using joins on another table
+-- this policy is to be used to lock down actions to only user_ids in the admin table
+CREATE policy "Members can do actions if they belong to the admin" 
+ON teams FOR on "public"."admin"
+AS PERMISSIVE
+FOR SELECT -- or UPDATE , DELETE, INSERT or ALL
+TO PUBLIC
+USING (
+    ( SELECT auth.uid ()) IN (SELECT user_id FROM "admin" WHERE user_id = id)
+);
+
+SELECT EXISTS (
+        SELECT 1
+        FROM admin
+        WHERE
+            user_id = auth.uid ()
+    );
+
+--   (select auth.uid()) in (select user_id from admin where team_id = id)
+
+-- --  based on joins on other tables
+-- CREATE policy "Policy for writing to readonly tables (mps,conties,constituencies,wards)" on "public"."admin" AS PERMISSIVE
+-- FOR UPDATE
+-- TO public
+-- using ( (SELECT auth.uid ()) IN (SELECT user_id FROM "admin" WHERE "user_id" = id));
+
+--  using postgres functions
 CREATE policy "Policy with security definer functions"
 on "public"."admin" 
 AS PERMISSIVE
