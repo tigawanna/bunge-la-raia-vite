899944098eb86ef3800dae037d96043795d83c18
tigawanna <denniskinuthiawaweru@gmail.com>
2024-07-15 19:25:39 +0300

    move navbar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { MainNavbar } from "@/components/MainNavbar";
+import { MainNavbar } from "@/components/navigation/MainNavbar";
 import { ParkuiToast } from "@/components/navigation/ParkuiToast";
 import { RecordAuthResponse } from "pocketbase";
 import { SupabaseUser, TypedSupabaseClient } from "@/lib/supabase/client";
