3c7aabb21e48a804b99bf88d8d2124bc2cae3ea2
tigawanna <denniskinuthiaw@gmail.com>
2024-07-22 21:42:07 +0300

    aspiration form

--- src/routes/candidates/$id/aspirations/form.tsx
+++ src/routes/candidates/$id/aspirations/form.tsx
@@ -13,6 +13,7 @@ export function CandidateAspirationFormPage({}:CandidateAspirationFormPageProps)
 return (
  <div className='w-full h-full flex flex-col items-center justify-center'>
   <h1 className='text-4xl'>candidate aspiration form</h1>
+  <AspirationsForm/>
  </div>
 );
 }
