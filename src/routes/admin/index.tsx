128a1c194752d37e14f689073158efa7ab6a4624
tigawanna <denniskinuthiawaweru@gmail.com>
2024-07-30 18:24:47 +0300

    add candidate summary generator

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -1,19 +1,35 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { AddCounties } from './-components/AddCounties';
+import { createFileRoute } from "@tanstack/react-router";
+import { AddCounties } from "./-components/AddCounties";
+import { supabase } from "@/lib/supabase/client";
 
-export const Route = createFileRoute('/admin/')({
-  component:AdminPage
-})
+export const Route = createFileRoute("/admin/")({
+  component: AdminPage,
+});
 
-interface AdminPageProps {
+interface AdminPageProps {}
 
+export function AdminPage({}: AdminPageProps) {
+  return (
+    <div className="w-full h-full min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-xl">Admin page</h1>
+      <AddCounties />
+    </div>
+  );
 }
 
-export function AdminPage({}:AdminPageProps){
-return (
- <div className='w-full h-full min-h-screen flex flex-col items-center justify-center'>
-  <h1 className='text-xl'>Admin page</h1>
-  <AddCounties/>
- </div>
+const { data: aspirations, error: aspirationError } = await supabase
+  .from("candidate_aspirations")
+  .select("*")
+  .eq("id", "uwu")
+  .select("*")
+  .order("created_at", { ascending: false })
+  .limit(20);
+const aspirations_text = aspirations?.reduce(
+  (acc, curr, idx) => {
+    const aspiration = `period: ${curr.period} vying_for: ${curr.vying_for} vying in ${curr.vying_in} 
+    mission statement: ${curr.mission_statement} vibe_check: ${JSON.stringify(curr.vibe_check)}`;
+    acc += `${idx + 1}. ${aspiration}\n`;
+    return acc;
+  },
+  ""
 );
-}
---
+++ supabase/functions/summarize-candidate/index.ts
@@ -0,0 +1,82 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { generateVibeSummary } from "../helpers/generate-vibe-summary.ts";
+import { createClient } from "jsr:@supabase/supabase-js";
+import { Database } from "../database.ts";
+import {
+  CandidateRecordType,
+  validateCandidate,
+} from "../helpers/validate-record.ts";
+
+interface RequestBody {
+  record: CandidateRecordType;
+}
+Deno.serve(async (req) => {
+  try {
+    const authHeader = req.headers.get("Authorization")!;
+    const supabaseClient = createClient<Database>(
+      Deno.env.get("SUPABASE_URL") ?? "",
+      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      { global: { headers: { Authorization: authHeader } } },
+    );
+    const { record } = (await req.json()) as RequestBody;
+    const raw_string_input = validateCandidate(record);
+    if (raw_string_input instanceof Response) {
+      return raw_string_input;
+    }
+
+    //  get candidate aspiration summary
+
+    const { data: aspirations } = await supabaseClient
+      .from("candidate_aspirations")
+      .select("*")
+      .eq("id", record.id)
+      .select("*")
+      .order("created_at", { ascending: false })
+      .limit(20);
+    const aspirations_text = aspirations?.reduce(
+      // @ts-expect-error supabase types are acting up here
+      (acc, curr, idx) => {
+        const aspiration =
+          `period: ${curr.period} vying_for: ${curr.vying_for} vying in ${curr.vying_in} 
+            mission statement: ${curr.mission_statement} vibe_check: ${
+            JSON.stringify(curr.vibe_check)
+          }`;
+        acc += `${idx + 1}. ${aspiration}\n`;
+        return acc;
+      },
+      "",
+    );
+
+    const summary = await generateVibeSummary({
+      inputText: raw_string_input + "\n" + aspirations_text,
+    });
+    const summary_text = summary.response.text();
+
+    if (!summary || !summary?.response) {
+      return new Response("candidate summary generation failed", {
+        status: 400,
+      });
+    }
+    const { data, error } = await supabaseClient.from("candidates")
+      .update({
+        candidate_summary: summary_text,
+      })
+      .eq("id", record.id)
+      .select("*")
+      .single();
+    if (error) {
+      return new Response(
+        "updating candidate summary failed :" + error.message,
+        { status: 400 },
+      );
+    }
+    return new Response(
+      "candidate summary " + data?.id + " updated successfully",
+      { status: 200 },
+    );
+  } catch (error) {
+    return new Response("Something went wrong: " + error.message, {
+      status: 500,
+    });
+  }
+});
