42be8be140f15cdb65852a801713bafc409f0338
tigawanna <denniskinuthiaw@gmail.com>
2024-08-01 06:43:33 +0300

    fix up candidate list

--- src/routes/candidates/-components/candidates/list/CandidatesList.tsx
+++ src/routes/candidates/-components/candidates/list/CandidatesList.tsx
@@ -25,23 +25,25 @@ export function CandidatesList({ q }: CandidatesListProps) {
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <ul className="w-full h-full flex flex-wrap items-center gap-2 p-2">
+      <ul className="w-full h-full flex flex-wrap items-center gap-2 p-2 
+      justify-center md:justify-start">
         {data.map((item) => {
           return (
             <Link
               to="/candidates/$candidate"
               params={{ candidate: item.id }}
               key={item.id}
-              className="w-[95%] sm:w-[45%] md:w-[30%] lg:w-[25%] p-2 bg-bg-muted rounded-lg flex flex-col gap-2">
-              <div className="w-full flex items-center justify-between ">
-                <h1 className="text-2xl">{item.name}</h1>
-              </div>
+              className="min-h-[300px] md:h-[300px] w-[95%] sm:w-[45%] md:w-[30%] lg:w-[25%] p-2 
+              bg-bg-muted rounded-lg flex flex-col  gap-2 hover:text-accent-text relative">
               <img
                 src={item.avatar_url ?? "unknown.jpg"}
                 alt={item.name}
-                className="h-[50%]l aspect-square"
+                className="size-full  object-cover rounded-lg absolute inset-0"
               />
-              <p className="text-sm line-clamp-4 p-1">{item.bio}</p>
+              <div className="w-full absolute z-20 bottom-0 left-0 right-0 p-2 bg-slate-900/50">
+                <h1 className="text-2xl">{item.name}</h1>
+              <p className="text-sm line-clamp-2 ">{item.bio}</p>
+              </div>
             </Link>
           );
         })}
