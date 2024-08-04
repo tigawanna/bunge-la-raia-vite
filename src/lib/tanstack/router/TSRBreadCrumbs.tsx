20e38f645634d6f9e6ca7a28c4a4df79cf935e05
tigawanna <denniskinuthiawaweru@gmail.com>
2024-08-04 17:26:25 +0300

    tweak bread crumbs

--- src/lib/tanstack/router/TSRBreadCrumbs.tsx
+++ src/lib/tanstack/router/TSRBreadCrumbs.tsx
@@ -8,14 +8,26 @@ export function TSRBreadCrumbs({}: TSRBreadCrumbsProps) {
   const { breadcrumb_routes } = useTSRBreadCrumbs();
 
   return (
-    <div className="w-full flex flex-wrap md:justify-end gap-0.1 p-1">
+    <div className="w-full flex flex-wrap md:justify-end gap-0.1 p-1 px-3">
       {breadcrumb_routes.map((crumb) => {
         if (breadcrumb_routes.length - 1 === breadcrumb_routes?.indexOf(crumb)) {
-          return <span key={crumb.path} className="text-xs bread-crumbs-links">{crumb.name}</span>;
+          return (
+            <span
+              key={crumb.path}
+              className="text-xs cursor-pointer line-clamp-1 
+              max-w-14 hover:max-w-fit hover:text-accent-text
+              hover:animate-in hover:fade-in  hover:duration-300">
+              {crumb.name}
+            </span>
+          );
         }
         return (
           <span key={crumb.path} className="flex gap-1 justify-center items-center">
-            <Link className="text-xs bread-crumbs-links" to={crumb.path}>
+            <Link
+              className="text-xs cursor-pointer line-clamp-1 
+              max-w-14 hover:max-w-fit hover:text-accent-text
+              hover:animate-in hover:fade-in hover:duration-300"
+              to={crumb.path}>
               {crumb.name}
             </Link>
             <ChevronRight className="size-4" />
