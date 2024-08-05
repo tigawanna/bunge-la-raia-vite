c50fa2a3404b290d3739cafde6d72e3e563c2113
tigawanna <denniskinuthiaw@gmail.com>
2024-08-05 15:37:28 +0300

    uwu

--- src/lib/tanstack/router/TSRBreadCrumbs.tsx
+++ src/lib/tanstack/router/TSRBreadCrumbs.tsx
@@ -6,17 +6,13 @@ interface TSRBreadCrumbsProps {}
 
 export function TSRBreadCrumbs({}: TSRBreadCrumbsProps) {
   const { breadcrumb_routes } = useTSRBreadCrumbs();
-
+  if(breadcrumb_routes.length<2)return null
   return (
     <div className="w-full flex flex-wrap md:justify-end gap-0.1 p-1 px-3">
       {breadcrumb_routes.map((crumb) => {
         if (breadcrumb_routes.length - 1 === breadcrumb_routes?.indexOf(crumb)) {
           return (
-            <span
-              key={crumb.path}
-              className="text-xs cursor-pointer line-clamp-1 
-              max-w-14 hover:max-w-fit hover:text-accent-text
-              hover:animate-in hover:fade-in  hover:duration-300">
+            <span key={crumb.path} className="text-xs bread-crumbs-links">
               {crumb.name}
             </span>
           );
--- src/lib/tanstack/router/use-bread-crumbs.ts
+++ src/lib/tanstack/router/use-bread-crumbs.ts
@@ -1,13 +1,12 @@
 import { useLocation } from "@tanstack/react-router";
 
-
 export function useTSRBreadCrumbs() {
   const current = useLocation();
-  // console.log("current === ",current);
+
   const route_history = current.pathname
     .split("/")
     .filter((x) => x && x.length > 0);
-  // console.log("route history ==== ",route_history)
+
   const breadcrumb_routes = route_history.reduce(
     (acc: { name: string; path: string }[], route) => {
       const prev_path = acc[acc.length - 1]?.path ?? "";
@@ -16,6 +15,5 @@ export function useTSRBreadCrumbs() {
     },
     [],
   );
-  // console.log("breadcrumbs routes  === ",breadcrumb_routes);
   return { breadcrumb_routes };
 }
