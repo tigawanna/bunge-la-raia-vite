7c7033c9672d3f319873b993854cabd3f77ef1ce
tigawanna <denniskinuthiaw@gmail.com>
2024-08-13 08:25:37 +0300

    candidate details form react hook form  -> tastack form

--- src/routes/candidates/-components/candidates/form/CandidateBasicDetailsForm.tsx
+++ src/routes/candidates/-components/candidates/form/CandidateBasicDetailsForm.tsx
@@ -1,13 +1,14 @@
-import { ResizeTextAreaFormField, TextFormField } from "@/lib/react-hook-form/TextFields";
 import { CandidateInsertType, CandidateRowType } from "../../types";
 import { useViewer } from "@/lib/tanstack/query/use-viewer";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { ImageURLInputField } from "@/lib/react-hook-form/ImageInput";
 import { useMutation } from "@tanstack/react-query";
 import { supabase } from "@/lib/supabase/client";
 import { MutationButton } from "@/lib/tanstack/query/MutationButton";
 import { toaster } from "@/components/navigation/ParkuiToast";
 import { DismissableAlert } from "@/components/wrappers/DismissableAlert";
+import { useForm } from "@tanstack/react-form";
+import { zodValidator } from "@tanstack/zod-form-adapter";
+import { ImageURLInputField, ResizeTextAreaFormField, TextFormField } from "@/lib/tanstack/form/TextFields";
+import { z } from "zod";
 
 interface CandidateBasicDetailsFormProps {
   next: (candidate: CandidateRowType) => void;
@@ -19,14 +20,6 @@ export function CandidateBasicDetailsForm({ candidate, next }: CandidateBasicDet
 
   const viewer = userQuery?.data?.data;
 
-  const { register, handleSubmit, formState, watch } = useForm<CandidateInsertType>({
-    defaultValues: {
-      name: candidate?.name ?? viewer?.fullname ?? "",
-      bio: candidate?.bio ?? viewer?.bio ?? "",
-      account_id: candidate?.account_id ?? viewer?.id ?? "",
-      avatar_url: candidate?.avatar_url ?? viewer?.avatar_url ?? "",
-    },
-  });
   const mutation = useMutation({
     mutationFn: async (data: CandidateInsertType) => {
       const { error } = await supabase.from("candidates").upsert(data).returns();
@@ -56,13 +49,28 @@ export function CandidateBasicDetailsForm({ candidate, next }: CandidateBasicDet
       invalidates: ["candidates", viewer?.id],
     },
   });
-  const onSubmit: SubmitHandler<CandidateInsertType> = (data) => {
-    mutation.mutate(data);
-  };
+  const form = useForm<CandidateInsertType>({
+    defaultValues: {
+      name: candidate?.name ?? viewer?.fullname ?? "",
+      bio: candidate?.bio ?? viewer?.bio ?? "",
+      account_id: candidate?.account_id ?? viewer?.id ?? "",
+      avatar_url: candidate?.avatar_url ?? viewer?.avatar_url ?? "",
+      banner_url: candidate?.banner_url ?? viewer?.banner_url ?? "",
+    },
+    onSubmit: async ({ value }) => {
+      await mutation.mutate(value);
+    },
+  });
   return (
     <div className="w-full h-full py-5 flex flex-col items-center justify-center ">
+
+
       <form
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
         className="w-[90%] md:w-[60%] lg:w-[50%] h-fit flex flex-col items-center bg-bg-emphasized justify-center p-[5%] lg:p-[3%] rounded-lg gap-4 overflow-auto">
         <h1 className="text-xl font-bold">Candidate Basic Details</h1>
 
@@ -70,27 +78,89 @@ export function CandidateBasicDetailsForm({ candidate, next }: CandidateBasicDet
           className=" text-sm bg-error text-error-content text-center"
           message="prefer not using your real name and images  to encourage your message to speak louder"
         />
+        <form.Field
+          name="name"
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChange: z.string(),
+          }}
+          children={(field) => {
+            return (
+              <TextFormField<CandidateInsertType>
+                field={field}
+                fieldKey="name"
+                fieldlabel="name"
+                inputOptions={{
+                  onBlur: field.handleBlur,
+                  onChange: (e) => field.handleChange(e.target.value),
+                }}
+              />
+            );
+          }}
+        />
 
-        {/* register your input into the hook by invoking the "register" function */}
-        <TextFormField<CandidateInsertType>
-          fieldKey="name"
-          formState={formState}
-          register={register}
-          required
+        <form.Field
+          name="bio"
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChange: z.string(),
+          }}
+          children={(field) => {
+            return (
+              <ResizeTextAreaFormField<CandidateInsertType>
+                field={field}
+                fieldKey="bio"
+                fieldlabel="Bio"
+                inputOptions={{
+                  onBlur: field.handleBlur,
+                  onChange: (e) => field.handleChange(e.target.value),
+                }}
+              />
+            );
+          }}
         />
-        <ResizeTextAreaFormField<CandidateInsertType>
-          fieldKey="bio"
-          formState={formState}
-          register={register}
-          required
+
+        <form.Field
+          name="avatar_url"
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChange: z.string(),
+          }}
+          children={(field) => {
+            return (
+              <ImageURLInputField<CandidateInsertType>
+                field={field}
+                fieldKey="avatar_url"
+                fieldlabel="Avatar url"
+                inputOptions={{
+                  onBlur: field.handleBlur,
+                  onChange: (e) => field.handleChange(e.target.value),
+                }}
+              />
+            );
+          }}
         />
-        <ImageURLInputField<CandidateInsertType>
-          fieldKey="avatar_url"
-          formState={formState}
-          register={register}
-          watch={watch}
-          required
+        <form.Field
+          name="banner_url"
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChange: z.string(),
+          }}
+          children={(field) => {
+            return (
+              <ImageURLInputField<CandidateInsertType>
+                field={field}
+                fieldKey="banner_url"
+                fieldlabel="Banner url"
+                inputOptions={{
+                  onBlur: field.handleBlur,
+                  onChange: (e) => field.handleChange(e.target.value),
+                }}
+              />
+            );
+          }}
         />
+
         <MutationButton label="Save" type="submit" mutation={mutation} />
       </form>
     </div>
--- src/routes/candidates/-components/types.ts
+++ src/routes/candidates/-components/types.ts
@@ -1,4 +1,35 @@
 import { Database } from "@/lib/supabase/db-types";
+import { VibeCheckType } from "@/lib/supabase/extra-db-types";
 
-export type CandidateRowType = Database["public"]["Tables"]["candidates"]["Row"];
-export type CandidateInsertType = Database["public"]["Tables"]["candidates"]["Insert"];
+// export type CandidateRowType = Database["public"]["Tables"]["candidates"]["Row"];
+// export type CandidateInsertType = Database["public"]["Tables"]["candidates"]["Insert"];
+
+export type CandidateRowType = {
+    account_id: string;
+    avatar_url: string | null;
+    banner_url: string | null;
+    bio: string | null;
+    candidate_summary: string | null;
+    created_at: string;
+    embedding: string | null;
+    id: string;
+    last_proompted_on: string | null;
+    name: string;
+    updated_at: string | null;
+    vibe_check?: VibeCheckType
+}
+
+export type CandidateInsertType = {
+    account_id?: string;
+    avatar_url?: string | null;
+    banner_url?: string | null;
+    bio?: string | null;
+    candidate_summary?: string | null;
+    created_at?: string;
+    embedding?: string | null;
+    id?: string;
+    last_proompted_on?: string | null;
+    name: string;
+    updated_at?: string | null;
+    vibe_check?:VibeCheckType
+}
